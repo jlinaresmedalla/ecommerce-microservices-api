b24a63ce7ba4c4fb730325f8d373c8f7172040fd
jlinaresmedalla <jlinaresmedalla@gmail.com>
2025-03-26 00:46:09 -0500

    feat: Implement command handlers for create product

--- src/Services/Catalog/Catalog.API/Products/CreateProduct/CreateProductHandler.cs
+++ src/Services/Catalog/Catalog.API/Products/CreateProduct/CreateProductHandler.cs
@@ -1,6 +1,30 @@
-﻿namespace Catalog.API.Products.CreateProduct
+﻿using Buildingblocks.CQRS;
+using Catalog.API.Models;
+
+namespace Catalog.API.Products.CreateProduct
 {
-    public class CreateProductHandler
+    public record CreateProductCommand(string Name, List<string> Categories, string Description, string ImageFile, double Price) : ICommand<CreateProductResult>;
+    public record CreateProductResult(Guid Id);
+
+    internal class CreateProductCommandHandler : ICommandHandler<CreateProductCommand, CreateProductResult>
     {
+        public async Task<CreateProductResult> Handle(CreateProductCommand command, CancellationToken cancellationToken)
+        {
+            // Create Product Entity from command data
+            var product = new Product
+            {
+                Name = command.Name,
+                Categories = command.Categories,
+                Description = command.Description,
+                ImageFile = command.ImageFile,
+                Price = command.Price
+            };
+
+            //TODO
+            // Save Product to Database
+
+            // Return Product Id
+            return new CreateProductResult(product.Id);
+        }
     }
 }
